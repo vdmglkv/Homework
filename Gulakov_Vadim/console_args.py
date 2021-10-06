200fe9d89c7155f098e703fbff48ed734b4a39ee
Vadim Gulakov <Haker.201@yandex.ru>
2021-10-06 20:47:40 +0300

    Implement class for newses and methods to print them

--- Gulakov_Vadim/console_args.py
+++ Gulakov_Vadim/console_args.py
@@ -1,5 +1,9 @@
 import argparse
 
+from parcer import ParserRSS
+
+rss_link = 'https://news.yahoo.com/rss/'
+
 parser = argparse.ArgumentParser(description='Pure Python command-line RSS reader')
 
 parser.add_argument('--version', nargs='?', const='1.4', type=str, help='Print version info')
@@ -8,3 +12,15 @@ parser.add_argument('--verbose', nargs='?', const=False, type=bool, help='Output
 parser.add_argument('--limit', nargs='?', const=1, type=int, help='Limit news topics if this parameter provided')
 
 args = parser.parse_args()
+pars = ParserRSS(rss_link)
+
+if __name__ == '__main__':
+    print('[INFO] Starting scraping')
+    print(pars.get_page_title())
+    newses = pars.get_news()
+    for news in newses[:args.limit]:
+        print('=' * 100)
+        news.get_info()
+        print('=' * 100)
+
+    print('[INFO] Finished scraping')
--- Gulakov_Vadim/parcer.py
+++ Gulakov_Vadim/parcer.py
@@ -1,48 +1,67 @@
 from bs4 import BeautifulSoup
 # from colorama import Fore
-from console_args import args
 import requests
 
-rss_link = 'https://news.yahoo.com/rss/'
+
+class News:
+
+    def __init__(self, title, link, published):
+        self.title = title
+        self.link = link
+        self.published = published
+
+    def get_info(self):
+        print(f'Title: {self.title}')
+        print(f'Data: {self.published}')
+        print(f'Link: {self.link}')
 
 
 class ParserRSS:
 
-    def __init__(self):
-        self.link = rss_link
-        self.version = args.version
-        self.json = args.json
-        self.verbose = args.verbose
-        self.limit = args.limit
+    def __init__(self, rss_url):
 
-    def parser(self, url):
+        self.session = requests.Session()
+        self.link = rss_url
 
-        s = requests.Session()
-        articles_list = []
+    def parse(self, tag='item'):
+
         try:
-            response = s.get(url)
+            response = self.session.get(self.link)
             soup = BeautifulSoup(response.content, features='xml')
-            articles = soup.findAll('item')
-
-            for a in articles:
-                title = a.find('title').text
-                link = a.find('link').text
-                published = a.find('pubDate').text
-
-                article = {
-                    'title': title,
-                    'link': link,
-                    'published': published
-                }
-                articles_list.append(article)
-
-            return articles_list
+            data = soup.findAll(tag)
+            return data
 
         except Exception as e:
             print('[INFO] The scraping job failed. See exception: ')
             print(e)
 
+    def get_page_title(self):
+        data = self.parse('channel')
+        for inf in data:
+            return inf.find('title').text
 
-print('Starting scraping')
-pars = ParserRSS().parser(rss_link)
-print('Finished scraping')
+    def get_news(self):
+
+        articles_list = []
+        articles = self.parse()
+
+        for a in articles:
+            title = a.find('title').text
+            link = a.find('link').text
+            published = a.find('pubDate').text
+
+            article = {
+                'title': title,
+                'link': link,
+                'published': published
+            }
+            news = News(**article)
+            articles_list.append(news)
+
+        return articles_list
+
+
+if __name__ == '__main__':
+    print('Starting scraping')
+
+    print('Finished scraping')
