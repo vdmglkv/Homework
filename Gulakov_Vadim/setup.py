c3d9860963ac48bf41c7fb9216f2fda1839c529a
Vadim Gulakov <Haker.201@yandex.ru>
2021-10-17 15:35:47 +0300

    Completed 2 iteration

---
+++ Gulakov_Vadim/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md") as file:
+    read_me_description = file.read()
+
+setuptools.setup(
+    name="rss_parser",
+    version="2.0",
+    author="Vadim Gulakov",
+    author_email="gulakovvadim2@gmail.com",
+    description="RSS-newses parser CLI utility.",
+    long_description=read_me_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vdmglkv/Homework",
+    packages=['rss_parser'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
